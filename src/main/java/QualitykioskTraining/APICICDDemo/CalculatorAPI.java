95b9d259aee868e38622bff671c8fd3fe4589bb6
Amit Deokar <amitdeokar20@gmail.com>
2021-05-15 11:52:03 +0530

    only addition available other files deleted

--- src/main/java/QualitykioskTraining/APICICDDemo/CalculatorAPI.java
+++ src/main/java/QualitykioskTraining/APICICDDemo/CalculatorAPI.java
@@ -8,22 +8,4 @@ public class CalculatorAPI
 		return Res;
 	}
 	
-	public int Subtraction(int num1,int num2)
-	{
-		int Res=num1-num2;
-		return Res;
-	}
-	
-	public int Division(int num1,int num2)
-	{
-		int Res=num1/num2;
-		return Res;
-	}
-	
-	public int Multiplication(int num1,int num2)
-	{
-		int R=num1*num2;
-		return R;
-	}
-
 }
